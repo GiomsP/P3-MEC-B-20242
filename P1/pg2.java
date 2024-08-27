938f194ac0008c09c097f0e6effbcf5dd4b4b30b
GiomsP <est.juan.dpalacios@unimilitar.edu.co>
2024-08-27 14:51:31 -0500

    Create pg2.java

---
+++ P1/pg2.java
@@ -0,0 +1,13 @@
+public class Fraccion {
+
+   private int numerador ;
+   private int denominador;
+   this.numerador = 0;
+   this.denominador>=1;
+   
+   
+}
+public class Fraccion (){
+   this.numerador = 0;
+   this.denominador>=1; 
+}

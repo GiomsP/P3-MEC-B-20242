6242fe34dac1ebfb3999d56a36648fee670e6324
GiomsP <est.juan.dpalacios@unimilitar.edu.co>
2024-08-27 14:35:41 -0500

    Create pg1.java

---
+++ P1/pg1.java
@@ -0,0 +1,24 @@
+public class Fraccion {
+
+    public static void main(String[] args) {
+        Fraccion.Numerador;
+        Fraccion.Denominador;
+      resultado=0;
+        a/b=resultado;
+    }
+}
+public static void Numerador.Fraccion {
+    
+    int a=0
+return a;
+}
+
+public static void Denominador {
+    
+    int b>0;
+    
+    return b;
+    
+}
+//Pregunta 1: Crear una clase llamada "fraccion", con atributos correspondientes a numerador y denominador.
+//Aplicar encapsulamiento y los metodos get y set adecuados para manipular la clase

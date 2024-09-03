95d07eaf08c36af3e02e7aa6ae298885ba19f213
GiomsP <est.juan.dpalacios@unimilitar.edu.co>
2024-09-02 19:21:42 -0500

    Update correccion-pg3.java

--- P1/correccion-pg3.java
+++ P1/correccion-pg3.java
@@ -1,1 +1,25 @@
+public class Triangulo {
+
+    private double base, altura;
+
+    public Triangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double calcularArea() {
+        return (base * altura) / 2;
+    }
+
+    public double calcularPerimetro() {
+        return base + altura + Math.sqrt(base * base + altura * altura);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Triangulo(3, 4).calcularArea());
+        System.out.println(new Triangulo(3, 4).calcularPerimetro());
+        System.out.println(new Triangulo(5, 12).calcularArea());
+        System.out.println(new Triangulo(5, 12).calcularPerimetro());
+    }
+}
 
